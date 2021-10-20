f5bf7ab8e30004e58ce0617419a3d6a70639e2bd
TurinDs2 <cperezd@unitec.edu>
2021-10-20 10:51:32 -0600

    Programación de Vehiculos en Arreglos Primitivos

---
+++ Programa1.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+public class Programa1{
+	public static void main(String []args){
+		JOptionPane.showMessageDialog(null,"Bienvenidos a Programación II!!!");
+	}
+}
